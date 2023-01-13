cdc65d32083075b34e47a00ba2d11606f7c965db
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-01-13 17:45:49 -0500

    remove post preview

--- components/PostPreview.jsx
+++ components/PostPreview.jsx
@@ -1,16 +1,15 @@
 import Date from './Date'
 import Link from 'next/link'
 
-export default function PostPreview({ title, date, excerpt, slug }) {
+export default function PostPreview({ title, date, slug }) {
   return (
     <div>
-      <h3 className="text-3xl font-bold mb-1 leading-snug">
-        <Link href={`/essays/${slug}`} className="hover:underline" dangerouslySetInnerHTML={{ __html: title }} />
+      <h3 className="text-3xl font-bold mb-1 leading-snug sm:leading-tight">
+        <Link href={`/essays/${slug}`} dangerouslySetInnerHTML={{ __html: title }} />
       </h3>
-      <div className="text-base font-medium mb-4">
+      <div className="font-light text-xs sm:text-sm">
         <Date dateString={date} />
       </div>
-      <div className="text-lg font-light leading-relaxed mb-4" dangerouslySetInnerHTML={{ __html: excerpt }} />
     </div>
   )
 }
