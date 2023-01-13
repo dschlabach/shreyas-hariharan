8c20f33de822edbeb8d7523f878d8784e167b74e
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-01-13 17:36:21 -0500

    small tweaks

--- components/ArticleTitle.tsx
+++ components/ArticleTitle.tsx
@@ -9,10 +9,9 @@ type ArticleTitleProps = {
 
 const ArticleTitle = ({ title, date, slug }: ArticleTitleProps) => {
   return (
-    <div className="flex flex-col gap-y-3 border-t border-ui-dark-purple relative pt-6">
-      <div className="absolute top-0 left-0 w-32 h-3 bg-ui-dark-purple" />
-      <p className="font-light text-sm">{date}</p>
-      <h3 className="-mt-1 font-bold text-2xl sm:text-3xl">
+    <div className="group flex flex-col gap-y-3 border-t border-ui-dark-purple relative pt-6 transition duration-75 ">
+      <p className="font-light text-xs sm:text-sm">{date}</p>
+      <h3 className="-mt-1 font-bold text-xl sm:text-3xl">
         <Link href={slug}>{title}</Link>
       </h3>
     </div>
