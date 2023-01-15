1070244b204d65814c9ce465840ea9931fc90df9
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-01-15 12:03:32 -0500

    fix twitter meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,8 +35,8 @@ export default function Home(props: HomeProps) {
       <main className="grid gap-y-16 sm:gap-y-24">
         <div className="flex flex-col font-light gap-y-4">
           <p className="text-base sm:text-lg leading-[140%]">
-            I'm a co-founder of Llama. We are contributing to a new economy powered by open-source software, permissionless capital
-            formation, and liquid labor.
+            I'm a co-founder of Llama. We are contributing to a new economy powered by open-source software,
+            permissionless capital formation, and liquid labor.
           </p>
           <Link href="/about" className="flex-shrink self-start border-b w-auto text-xs sm:text-sm">
             More about me.
