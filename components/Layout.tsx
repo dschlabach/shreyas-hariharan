d7ef73b7d61e69f2e7c2f4c86bd727c46573828b
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-02-19 12:18:34 -0500

    turn off dark mode by default

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,22 +3,7 @@ import Header from 'components/Header'
 import { useDarkMode } from 'usehooks-ts'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { isDarkMode, toggle, enable, disable } = useDarkMode()
-
-  React.useEffect(() => {
-    if (window?.matchMedia !== undefined) {
-      // Add listener to update styles
-      window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => enable())
-
-      // Setup dark/light mode for the first time
-      window.matchMedia('(prefers-color-scheme: dark)').matches ? () => enable() : () => disable()
-
-      // Remove listener
-      return () => {
-        window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', () => {})
-      }
-    }
-  }, [])
+  const { isDarkMode, toggle } = useDarkMode()
 
   return (
     <div className={`${isDarkMode ? ' dark' : ''}`}>
