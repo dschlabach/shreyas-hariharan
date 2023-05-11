73a28a65bbd0648cb168479459109fd545e273c3
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-05-11 10:33:56 -0400

    update header

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,8 +35,7 @@ export default function Home(props: HomeProps) {
       <main className="grid gap-y-16 sm:gap-y-24">
         <div className="flex flex-col font-light gap-y-4">
           <p className="text-base sm:text-lg leading-[140%] dark:text-white/80 text-primary/80">
-            I'm the cofounder of Llama. We are contributing to a new economy powered by open-source software,
-            permissionless capital formation, and liquid labor.
+            I'm the cofounder of Llama, a governance system for onchain organizations.
           </p>
           <Link
             href="/about"
