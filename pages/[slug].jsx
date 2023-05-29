90430dcd58aa6709c3b89cb344f433dbcac8bfca
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-05-29 19:29:21 -0400

    fix og:image

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -24,6 +24,7 @@ export default function Post({ post }) {
             <Head>
               <title>{post.title} | Shreyas Hariharan</title>
               <meta property="og:title" content={`${post.title} | Shreyas Hariharan`} key="title" />
+              <meta property="og:image" content={post.featuredImage?.node.sourceUrl} />
               <meta property="og:twitter" content={`${post.title} | Shreyas Hariharan`} key="title" />
               <meta property="twitter:title" content={`${post.title} | Shreyas Hariharan`} key="title" />
               <meta property="twitter:image" content={post.featuredImage?.node.sourceUrl} />
