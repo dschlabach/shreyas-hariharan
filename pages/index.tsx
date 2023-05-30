3617d12abe49ea335202f00d7caaed3ead99f7d9
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-05-29 21:01:08 -0400

    test 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,8 +28,8 @@ export default function Home(props: HomeProps) {
     <div>
       <Head>
         <title>Shreyas Hariharan</title>
-        <meta name="description" content="Shreyas Hariharan, test" key="title" />
-        <meta name="og:title" content="Shreyas Hariharan" key="title" />
+        <meta name="description" content="Shreyas Hariharan, test" key="description" />
+        <meta name="og:title" content="Shreyas Hariharan, testing2" key="title" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
