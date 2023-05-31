82ffc962e5e0cc41742cbcfe8e45069a6c1fbd1f
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-05-31 12:18:16 -0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
-        <meta charSet="utf-8" />
         <title>Shreyas Hariharan</title>
         <meta property="og:title" content="Shreyas Hariharan" key="title" />
         <meta property="twitter:title" content="Shreyas Hariharan" key="title" />
