2f1431e489f031d55fd9e0ebcf0c26b20826712a
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-05-31 12:22:43 -0400

    move head out of layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,15 @@ import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
       <Head>
         <title>Shreyas Hariharan</title>
         <meta property="og:title" content="Shreyas Hariharan" key="title" />
         <meta property="twitter:title" content="Shreyas Hariharan" key="title" />
       </Head>
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
