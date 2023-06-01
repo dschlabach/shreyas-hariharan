1fcfc14ced5c2666b6ad4a917eb82d08f49cee19
Daniel Schlabach <daniel.schlabach3@gmail.com>
2023-06-01 07:04:07 -0400

    add author and site for twitter

--- pages/[slug].jsx
+++ pages/[slug].jsx
@@ -22,10 +22,13 @@ export default function Post({ post }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={`${post?.title} | Shreyas Hariharan`} key="title" />
         <meta property="og:image" content={post?.featuredImage?.node.sourceUrl} key="image" />
+
+        <meta name="twitter:card" content="summary_large_image" />
         <meta property="og:twitter" content={`${post?.title} | Shreyas Hariharan`} />
         <meta property="twitter:title" content={`${post?.title} | Shreyas Hariharan`} />
         <meta property="twitter:image" content={post?.featuredImage?.node?.sourceUrl} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:site" content="@HelloShreyas" />
+        <meta property="twitter:creator" content="@HelloShreyas" />
       </Head>
       <div>
         {router.isFallback ? (
